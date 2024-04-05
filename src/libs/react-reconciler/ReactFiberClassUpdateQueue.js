5d635df4f25715c8ca88fbeac72989eb48c3fdba
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-05 10:02:31 +0800

    Transform the processUpdateQueue function, only applicable to the first rendering

--- src/libs/react-reconciler/ReactFiberClassUpdateQueue.js
+++ src/libs/react-reconciler/ReactFiberClassUpdateQueue.js
@@ -38,3 +38,91 @@ export function enqueueUpdate(fiber, update) {
   const sharedQueue = updateQueue.shared;
   return enqueueConcurrentClassUpdate(fiber, sharedQueue, update);
 }
+
+export function finishQueueingConcurrentUpdates() {
+  const endIndex = concurrentQueuesIndex;
+  concurrentQueuesIndex = 0;
+
+  let i = 0;
+  while (i < endIndex) {
+    const fiber = concurrentQueues[i];
+    concurrentQueues[i++] = null;
+    const queue = concurrentQueues[i];
+    concurrentQueues[i++] = null;
+    const update = concurrentQueues[i];
+    concurrentQueues[i++] = null;
+    // const lane = concurrentQueues[i];
+    // concurrentQueues[i++] = null;
+
+    if (queue !== null && update !== null ) {
+      const pending = queue.pending;
+      if (pending === null) {
+        update.next = update;
+      } else {
+        update.next = pending.next;
+        pending.next = update;
+      }
+      queue.pending = update;
+    }
+  }
+}
+
+export function cloneUpdateQueue(current, wip) {
+  const queue = wip.updateQueue;
+  const currentQueue = current.updateQueue;
+  if (queue === currentQueue) {
+    const clone = {
+      baseState: currentQueue.baseState,
+      firstBaseUpdate: currentQueue.firstBaseUpdate,
+      lastBaseUpdate: currentQueue.lastBaseUpdate,
+      shared: currentQueue.shared,
+      callbacks: null,
+    };
+
+    wip.updateQueue = clone;
+  }
+}
+
+export function processUpdateQueue(wip, props) {
+  const queue = wip.updateQueue;
+  let newState = queue.baseState;
+  newState = getStateFromUpdate(
+    workInProgress,
+    queue,
+    update,
+    newState,
+    props,
+    null,
+  );
+
+  wip.memoizedState = newState;
+}
+
+function getStateFromUpdate(
+  wip,
+  queue,
+  update,
+  prevState,
+  nextProps,
+  instance,
+) {
+  switch (update.tag) {
+    case UpdateState: {
+      const payload = update.payload;
+      let partialState;
+      if (typeof payload === 'function') {
+        partialState = payload.call(instance, prevState, nextProps);
+      } else {
+        partialState = payload;
+      }
+      if (partialState === null || partialState === undefined) {
+        // Null and undefined are treated as no-ops.
+        return prevState;
+      }
+      // Merge the partial state and the previous state.
+      return assign({}, prevState, partialState);
+    }
+  }
+
+  return prevState;
+}
