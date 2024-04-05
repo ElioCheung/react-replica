ef148f8b87dc9dbd40507c0ae8ef19b820c14a36
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-05 09:54:34 +0800

    Turn on sourceMap to facilitate local vscode debugging

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ const config = {
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
+  devtool: "source-map",
   module: {
     rules: [
       {
