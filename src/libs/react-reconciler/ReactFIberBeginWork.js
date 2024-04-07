87775655a464f4dcfcc23fd9d354d28459383e6f
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-07 16:04:40 +0800

    Complete the updateHostRoot function function

--- src/libs/react-reconciler/ReactFIberBeginWork.js
+++ src/libs/react-reconciler/ReactFIberBeginWork.js
@@ -1,4 +1,6 @@
 import { cloneUpdateQueue, processUpdateQueue } from './ReactFiberClassUpdateQueue';
+import { reconcileChildFibers } from './ReactChildFiber';
+import { HostRoot } from './ReactWorkTags';
 
 export function beginWork(current, wip) {
   if (current !== null) {
@@ -16,11 +18,10 @@ export function beginWork(current, wip) {
 
 function updateHostRoot(current, wip) {
   const nextProps = wip.pendingProps;
-  const prevState = wip.memoizedState;
-  const prevChildren = prevState.element;
-  
-  cloneUpdateQueue(current, wip);
-  // Transform the processUpdateQueue function, only applicable to the first rendering
+  // fixme: wip's updateQueue is assigned from HostRootFiber, why does it need to be cloned?
+  // cloneUpdateQueue(current, wip);
+
+  // Simplify the processUpdateQueue function. Its function when rendering for the first time is to take out the updateQueue.shared.pending value and copy it to memoizedState
   processUpdateQueue(wip, nextProps, null);
 
   const nextState = wip.memoizedState;
@@ -32,8 +33,17 @@ function updateHostRoot(current, wip) {
 
 export function reconcileChildren(current, wip, nextChildren) {
   if (current === null) {
-
+    // If this is a fresh new component that hasn't been rendered yet, we
+    // won't update its child set by applying minimal side-effects. Instead,
+    // we will add them all to the child before it gets rendered. That means
+    // we can optimize this reconciliation pass by not tracking side-effects.
   } else {
+    // If the current child is the same as the work in progress, it means that
+    // we haven't yet started any work on these children. Therefore, we use
+    // the clone algorithm to create a copy of all the current children.
 
+    // If we had any progressed work already, that is invalid at this point so
+    // let's throw it out.
+    wip.child = reconcileChildFibers(wip, current.child, nextChildren);
   }
 }
