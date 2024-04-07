1dce77b04ba0f2362621ec73f6550be7ef10e5f3
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-07 20:06:40 +0800

    Determine whether the type is text content

---
+++ src/libs/react-dom/client/ReactDOMHostConfig.js
@@ -0,0 +1,11 @@
+export function shouldSetTextContent(type, props) {
+  return (
+    type === 'textarea' ||
+    type === 'noscript' ||
+    typeof props.children === 'string' ||
+    typeof props.children === 'number' ||
+    (typeof props.dangerouslySetInnerHTML === 'object' &&
+      props.dangerouslySetInnerHTML !== null &&
+      props.dangerouslySetInnerHTML.__html != null)
+  );
+}
