9967a0c71d1bd797c91df19c68c3a090324e7e7d
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-08 10:47:16 +0800

    Set the text content value of the HTML node

---
+++ src/libs/react-dom/client/setTextContent.js
@@ -0,0 +1,17 @@
+import { TEXT_NODE } from '../../shared/HTMLNodeType';
+
+export function setTextContent(node, text) {
+  if (text) {
+    const firstChild = node.firstChild;
+
+    if (
+      firstChild &&
+      firstChild === node.lastChild &&
+      firstChild.nodeType === TEXT_NODE
+    ) {
+      firstChild.nodeValue = text;
+      return;
+    }
+  }
+  node.textContent = text;
+}
