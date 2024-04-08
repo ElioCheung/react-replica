eb1ad3f7d72958984dfc8d7074dbdab98f635fa7
ElioCheung <elio.cheung.cn@gmail.com>
2024-04-08 10:46:23 +0800

    Store fiber and props information on the container node

--- src/libs/react-dom-bindings/src/client/ReactDOMComponentTree.js
+++ src/libs/react-dom-bindings/src/client/ReactDOMComponentTree.js
@@ -1,6 +1,16 @@
 const randomKey = Math.random().toString(36).slice(2);
+const internalInstanceKey = '__reactFiber$' + randomKey;
+const internalPropsKey = '__reactProps$' + randomKey;
 const internalContainerInstanceKey = '__reactContainer$' + randomKey;
 
 export function markContainerAsRoot(fiberRootNode, container) {
   container[internalContainerInstanceKey] = fiberRootNode;
+}
+
+export function precacheFiberNode(hostInst, node) {
+  node[internalInstanceKey] = hostInst;
+}
+
+export function updateFiberProps(node, props) {
+  node[internalPropsKey] = props;
 }
